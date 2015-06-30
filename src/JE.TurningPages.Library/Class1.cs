19079930af1cec7ce3f997e12aa5603af6b5c067
Peter Mounce <peter.mounce@just-eat.com>
2015-06-30 09:26:46 +0100

    Initial template

---
+++ src/JE.TurningPages.Library/Class1.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JE.TurningPages.Library
+{
+    public class Class1
+    {
+    }
+}
