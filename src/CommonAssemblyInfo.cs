fee04e6b3a7413e75bb656ba36e04a77f6477bc2
Peter Mounce <peter.mounce@just-eat.com>
2015-08-05 13:41:10 +0100

    Add author token replacement

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -1,5 +1,5 @@
 using System.Reflection;
 
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyDescription("Library to support link-header-based paging per RFC-5988")]
+[assembly: AssemblyCompany("JUST EAT plc")]
 [assembly: AssemblyInformationalVersion("1.0.0.0")]
