854f3b0d34a19d19c23537075eecf7be401371d4
Peter Mounce <peter.mounce@just-eat.com>
2015-08-11 22:49:12 +0100

    tests to repro #1
    @payman81 - is this what you mean? These tests fail - but for the same reason as your issue?

--- src/JE.TurningPages.Examples.Tests/describe_paging.cs
+++ src/JE.TurningPages.Examples.Tests/describe_paging.cs
@@ -1,22 +1,36 @@
 using System;
+using System.Linq;
+using System.Net;
 using System.Net.Http;
-using System.Threading.Tasks;
 using Shouldly;
 
 namespace JE.TurningPages.Examples.Tests
 {
     public class describe_paging : when_making_requests
     {
-        public async Task when_request_is_successful()
+        public void when_request_is_to_simple_waiting_action_that_does_not_page()
         {
-            act = async () => Response = await App.CreateRequest("/widgets").GetAsync().ConfigureAwait(false);
+            act = () => Response = App.CreateRequest("/api/waiting").GetAsync().Result;
+            it["should give 200 OK"] = () => Response.StatusCode.ShouldBe(HttpStatusCode.OK);
+            it["should not have a link header"] = () => ShouldNotHaveHeader(Response, "link");
+            it["should not have a pagination header"] = () => ShouldNotHaveHeader(Response, "x-pagination");
+        }
+
+        public void when_request_is_to_operation_that_pages()
+        {
+            act = () => Response = App.CreateRequest("/api/widgets").GetAsync().Result;
             it["should have a link header"] = () => ShouldHaveHeader(Response, "link");
             it["should have a pagination header"] = () => ShouldHaveHeader(Response, "x-pagination");
         }
 
+        private static void ShouldNotHaveHeader(HttpResponseMessage response, string header)
+        {
+            response.Headers.ShouldNotContain(x => x.Key.Equals(header, StringComparison.InvariantCultureIgnoreCase), () => string.Join("\n", response.Headers.Select(y => y.Key + " " + y.Value)));
+        }
+
         private static void ShouldHaveHeader(HttpResponseMessage message, string header)
         {
-            message.Headers.ShouldContain(x => x.Key.Equals(header, StringComparison.InvariantCultureIgnoreCase));
+            message.Headers.ShouldContain(x => x.Key.Equals(header, StringComparison.InvariantCultureIgnoreCase), () => string.Join("\n", message.Headers.Select(y => y.Key + " " + y.Value)));
         }
     }
 }
---
+++ src/JE.TurningPages.Examples.WebApi/Widgets/WaitingController.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace JE.TurningPages.Examples.WebApi.Widgets
+{
+    public class WaitingController : ApiController
+    {
+        [HttpGet]
+        public async Task<IHttpActionResult> Search()
+        {
+            var result = await Task.Factory.StartNew(() => Task.Delay(1000));
+            return Ok();
+        }
+    }
+}
