0bd14b53a0a5dbac8d370a922d5093baa2fe8295
Peter Mounce <peter.mounce@just-eat.com>
2015-08-11 23:30:05 +0100

    eliminate the .Result, make tests async/await

--- src/JE.TurningPages.Examples.Tests/describe_paging.cs
+++ src/JE.TurningPages.Examples.Tests/describe_paging.cs
@@ -1,10 +1,12 @@
+using System.Threading.Tasks;
+
 namespace JE.TurningPages.Examples.Tests
 {
     public class describe_paging : when_making_requests
     {
-        public void to_operation_that_pages()
+        public async Task to_operation_that_pages()
         {
-            act = () => Response = App.CreateRequest("/api/widgets").GetAsync().Result;
+            act = async () => Response = await App.CreateRequest("/api/widgets").GetAsync().ConfigureAwait(true);
             it["should have a link header"] = () => ShouldHaveHeader(Response, "link");
             it["should have a pagination header"] = () => ShouldHaveHeader(Response, "x-pagination");
         }
--- src/JE.TurningPages.Examples.Tests/does_not_deadlock.cs
+++ src/JE.TurningPages.Examples.Tests/does_not_deadlock.cs
@@ -1,13 +1,14 @@
 using System.Net;
+using System.Threading.Tasks;
 using Shouldly;
 
 namespace JE.TurningPages.Examples.Tests
 {
     public class does_not_deadlock : when_making_requests
     {
-        public void to_simple_waiting_action_that_does_not_page()
+        public async Task to_simple_waiting_action_that_does_not_page()
         {
-            act = () => Response = App.CreateRequest("/api/waiting").GetAsync().Result;
+            act = async () => Response = await App.CreateRequest("/api/waiting").GetAsync().ConfigureAwait(true);
             it["should give 200 OK"] = () => Response.StatusCode.ShouldBe(HttpStatusCode.OK);
             it["should not have a link header"] = () => ShouldNotHaveHeader(Response, "link");
             it["should not have a pagination header"] = () => ShouldNotHaveHeader(Response, "x-pagination");
